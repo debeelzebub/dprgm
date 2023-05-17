39956cd64750410dd02beaf36331c4cb72d91b18
debeelzebub <sruthysurendrants@gmail.com>
2023-05-17 12:30:14 +0530

    change

---
+++ bin/mathopr.dart
@@ -0,0 +1,25 @@
+void add(){
+  int a=10,b=30;
+  print("sum = ${a+b}");
+}
+
+void sub(){
+  int a=25,b=5;
+  print("sum = ${a-b}");
+}
+void multi(){
+  int a=25,b=5;
+  print("sum = ${a*b}");
+}
+
+void div(){
+  int a=25,b=5;
+  print("sum = ${a/b}");
+}
+
+void main(){
+  add();
+  sub();
+  multi();
+  div();
+}
